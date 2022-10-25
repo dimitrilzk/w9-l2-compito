a80b7894196a368fc59bfd0314e81b653a583d3f
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-25 15:48:21 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+class Conto {
+    constructor(saldoAttuale, versamento, prelievo) {
+        this.saldoAttuale = saldoAttuale;
+        this.versamento = versamento;
+        this.prelievo = prelievo;
+    }
+    getSaldoAttuale() {
+        return this.saldoAttuale;
+    }
+    getVersamento() {
+        return this.versamento;
+    }
+    getPrelievo() {
+        return this.prelievo;
+    }
+}
+let figlio = new Conto(500, 0, 0);
+console.log(figlio);
+let madre = new Conto(5000, 1200, 500);
+console.log(madre);
---
+++ ts/script.ts
@@ -0,0 +1,25 @@
+class Conto {
+    private saldoAttuale: number; 
+    private versamento: number;
+    private prelievo: number; 
+
+    constructor (saldoAttuale: number, versamento: number, prelievo: number ) {
+        this.saldoAttuale = saldoAttuale;
+        this.versamento = versamento;
+        this.prelievo = prelievo;
+    }
+    
+    public getSaldoAttuale(): number {
+        return this.saldoAttuale;
+    }
+    public getVersamento(): number {
+        return this.versamento;
+    }
+    public getPrelievo(): number {
+        return this.prelievo;
+    }
+}
+let figlio = new Conto(500, 0, 0)
+console.log(figlio);
+let madre = new Conto(5000, 1200, 500)
+console.log(madre);
